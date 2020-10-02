af9c73f9864ecdd5b4457b890ef8369be12f9d39
Jose Quezada <quezada007@yahoo.com>
2020-10-02 07:00:56 -0700

    Add Bubble Sort unit tests

---
+++ src/algorithms/sorting/bubble-sort.test.js
@@ -0,0 +1,19 @@
+import bubbleSort from './bubble-sort';
+
+describe('Bubble Sort', () => {
+    test('sort array of numbers', () => {
+        const unsortedArray = [3, 5, 4, 6, 7, 2, 1];
+        const expectedResult = [1, 2, 3, 4, 5, 6, 7];
+        const result = bubbleSort(unsortedArray);
+        expect(result).toEqual(expectedResult);
+        expect(result).not.toBe(expectedResult);
+    });
+
+    test('sort array of strings', () => {
+        const unsortedArray = ['dog', 'cat', 'zebra', 'panda', 'bear', 'elephant', 'tiger', 'lion'];
+        const expectedResult = ['bear', 'cat', 'dog', 'elephant', 'lion', 'panda', 'tiger', 'zebra'];
+        const result = bubbleSort(unsortedArray);
+        expect(result).toEqual(expectedResult);
+        expect(result).not.toBe(expectedResult);
+    });
+});
