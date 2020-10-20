7055cdddd89af5c1f5eb3e260c1d34f05d89d3c0
Jose Quezada <quezada007@yahoo.com>
2020-10-19 20:30:06 -0700

    Add comments to Singly Linked List classes and contructors

--- src/data-structures/linked-lists/singly-linked-list.js
+++ src/data-structures/linked-lists/singly-linked-list.js
@@ -1,12 +1,25 @@
 /* eslint-disable max-classes-per-file */
+/**
+ * Class representing a node for the Singly Linked List.
+ */
 export class Node {
+    /**
+     * Create a node.
+     * @param {string|number} data - The data for the node.
+     */
     constructor(data) {
         this.data = data;
         this.next = null;
     }
 }
 
+/**
+ * Class representing a Singly Linked List.
+ */
 export default class SinglyLinkedList {
+    /**
+     * Create a Singly Linked List.
+     */
     constructor() {
         this.head = null;
         this.length = 0;
