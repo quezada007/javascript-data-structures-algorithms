27a533e5d630245ec93087f16216bf61bfd57c1e
Jose Quezada <quezada007@yahoo.com>
2020-11-14 06:29:42 -0800

    add insertLast method for Doubly Linked List

--- src/data-structures/linked-lists/doubly-linked-list.js
+++ src/data-structures/linked-lists/doubly-linked-list.js
@@ -42,6 +42,26 @@ class DoublyLinkedList {
         }
         this.length += 1;
     }
+
+    /**
+     * Add a new node at the end of the linked list.
+     * @param {string|number} data - The data for the node.
+     */
+    insertLast(data) { // O(n)
+        const newNode = new Node(data);
+        // Check to see if the list is empty
+        if (this.head === null) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = newNode;
+            newNode.prev = current;
+        }
+        this.length += 1;
+    }
 }
 
 export default DoublyLinkedList;
