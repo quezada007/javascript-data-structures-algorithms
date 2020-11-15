da33b0a13d366e098ee7f611b0fdae1ac41cff8b
Jose Quezada <quezada007@yahoo.com>
2020-11-15 10:33:55 -0800

    add unit tests for insertLast method

--- src/data-structures/linked-lists/doubly-linked-list.test.js
+++ src/data-structures/linked-lists/doubly-linked-list.test.js
@@ -21,4 +21,25 @@ describe('Doubly Linked List', () => {
         expect(list.head.next.next.prev.prev.data).toEqual('c');
         expect(list.head.next.next.prev.prev.prev).toEqual(null);
     });
+
+    test('inserting a node at the end of the list using the insertLast method', () => {
+        const list = new DoublyLinkedList();
+        expect(list.head).toEqual(null);
+        list.insertLast('a');
+        expect(list.head.data).toEqual('a');
+        list.insertLast('b');
+        expect(list.head.data).toEqual('a');
+        expect(list.head.next.data).toEqual('b');
+        expect(list.head.next.prev.data).toEqual('a');
+        list.insertLast('c');
+        expect(list.head.data).toEqual('a');
+        expect(list.head.next.data).toEqual('b');
+        expect(list.head.next.prev.data).toEqual('a');
+        expect(list.head.next.next.data).toEqual('c');
+        expect(list.head.next.next.prev.data).toEqual('b');
+        expect(list.head.next.next.next).toEqual(null);
+        expect(list.head.next.next.prev.data).toEqual('b');
+        expect(list.head.next.next.prev.prev.data).toEqual('a');
+        expect(list.head.next.next.prev.prev.prev).toEqual(null);
+    });
 });
