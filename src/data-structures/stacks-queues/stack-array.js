4c50521a80a77bea3e370e8190b3612c13af2fa0
Jose Quezada <quezada007@yahoo.com>
2020-11-20 07:23:27 -0800

    add isEmpty method to Stack Array

--- src/data-structures/stacks-queues/stack-array.js
+++ src/data-structures/stacks-queues/stack-array.js
@@ -8,6 +8,14 @@ class StackArray {
     constructor() {
         this.stack = [];
     }
+
+    /**
+     * Check the stack to see if it's empty.
+     * @returns {boolean} - true if the list is empty or false if not.
+     */
+    isEmpty() { // O(1)
+        return this.stack.length === 0;
+    }
 }
 
 export default StackArray;
