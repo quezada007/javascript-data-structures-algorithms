bf329f0ef3ef9a88131e36bf81b504ecaec116d8
Jose Quezada <quezada007@yahoo.com>
2020-11-20 07:22:35 -0800

    create Stack Array data structure

---
+++ src/data-structures/stacks-queues/stack-array.js
@@ -0,0 +1,13 @@
+/**
+ * Class representing a Stack using an array.
+ */
+class StackArray {
+    /**
+     * Create a Stack Array.
+     */
+    constructor() {
+        this.stack = [];
+    }
+}
+
+export default StackArray;
