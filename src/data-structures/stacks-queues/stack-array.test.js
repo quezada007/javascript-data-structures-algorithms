9d2e7c9dab5896d798de4fc0132c9e86a280423b
Jose Quezada <quezada007@yahoo.com>
2020-11-21 07:26:32 -0800

    add unit tests to pop method

--- src/data-structures/stacks-queues/stack-array.test.js
+++ src/data-structures/stacks-queues/stack-array.test.js
@@ -14,4 +14,20 @@ describe('Stack Array', () => {
         expect(stack.stack[1]).toEqual('b');
         expect(stack.stack[2]).toEqual('c');
     });
+
+    test('removing from the stack using the pop method', () => {
+        const stack = new StackArray();
+        expect(stack.stack).toHaveLength(0);
+        expect(() => { stack.pop(); }).toThrow();
+        stack.push('a');
+        expect(stack.pop()).toEqual('a');
+        stack.push('a');
+        stack.push('b');
+        stack.push('c');
+        stack.push('d');
+        expect(stack.pop()).toEqual('d');
+        expect(stack.pop()).toEqual('c');
+        expect(stack.pop()).toEqual('b');
+        expect(stack.pop()).toEqual('a');
+    });
 });
