1cce8d33c05342a4bf8a44fae81a52bb524aaed3
Jose Quezada <quezada007@yahoo.com>
2020-11-22 09:02:44 -0800

    add unit tests for pop method

--- src/data-structures/stacks-queues/stack-linked-list.test.js
+++ src/data-structures/stacks-queues/stack-linked-list.test.js
@@ -14,4 +14,20 @@ describe('Stack Linked List', () => {
         expect(stack.head.next.data).toEqual('b');
         expect(stack.head.next.next.data).toEqual('a');
     });
+
+    test('removing from the stack using the pop method', () => {
+        const stack = new StackLinkedList();
+        expect(stack.head).toBeNull();
+        expect(() => { stack.pop(); }).toThrow();
+        stack.push('a');
+        expect(stack.pop()).toEqual('a');
+        stack.push('a');
+        stack.push('b');
+        stack.push('c');
+        stack.push('d');
+        expect(stack.pop()).toEqual('d');
+        expect(stack.pop()).toEqual('c');
+        expect(stack.pop()).toEqual('b');
+        expect(stack.pop()).toEqual('a');
+    });
 });
