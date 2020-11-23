7dd10cdd4694f7b03456e9b8c75caac9a83cb4f4
Jose Quezada <quezada007@yahoo.com>
2020-11-23 06:57:21 -0800

    add isEmpty method to Queue Array

--- src/data-structures/stacks-queues/queue-array.js
+++ src/data-structures/stacks-queues/queue-array.js
@@ -8,6 +8,14 @@ class QueueArray {
     constructor() {
         this.queue = [];
     }
+
+    /**
+     * Check the queue to see if it's empty.
+     * @returns {boolean} - true if the list is empty or false if not.
+     */
+    isEmpty() { // O(1)
+        return this.queue.length === 0;
+    }
 }
 
 export default QueueArray;
