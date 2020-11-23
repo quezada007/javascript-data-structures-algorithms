655501727275c46730fc9a92dae2b060b97840ba
Jose Quezada <quezada007@yahoo.com>
2020-11-23 06:56:35 -0800

    create Queue Array

---
+++ src/data-structures/stacks-queues/queue-array.js
@@ -0,0 +1,13 @@
+/**
+ * Class representing a Queue using an array.
+ */
+class QueueArray {
+    /**
+     * Create a Queue Array.
+     */
+    constructor() {
+        this.queue = [];
+    }
+}
+
+export default QueueArray;
