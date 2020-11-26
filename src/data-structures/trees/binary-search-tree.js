a2cab3a3aa94934017548a44a9bb1cf557eeec52
Jose Quezada <quezada007@yahoo.com>
2020-11-26 12:09:48 -0800

    add insert method to Binary Search Tree

--- src/data-structures/trees/binary-search-tree.js
+++ src/data-structures/trees/binary-search-tree.js
@@ -25,6 +25,40 @@ class BinarySearchTree {
     constructor() {
         this.root = null;
     }
+
+    /**
+     * Insert a new node to the tree.
+     * @param {string|number} data - The data for the node.
+     */
+    insert(data) { // O(log n)
+        const newNode = new Node(data);
+        // If the treen is empty, insert at the root
+        if (this.root === null) {
+            this.root = newNode;
+            return;
+        }
+        let current = this.root;
+        // The tree is not empty
+        while (true) {
+            // Check for duplicate Node values
+            if (data === current.data) {
+                break;
+            }
+            if (data < current.data) {
+                if (current.left === null) {
+                    current.left = newNode;
+                    break;
+                }
+                current = current.left;
+            } else {
+                if (current.right === null) {
+                    current.right = newNode;
+                    break;
+                }
+                current = current.right;
+            }
+        }
+    }
 }
 
 export default BinarySearchTree;
