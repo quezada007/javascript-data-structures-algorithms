23689e7a498173dae0baac8722d3ede5d25ccaf6
Jose Quezada <quezada007@yahoo.com>
2020-11-29 09:37:10 -0800

    create Max Binary Heap

---
+++ src/data-structures/trees/max-binary-heap.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-constant-condition */
+/**
+ * Class representing a Max Binary Heap.
+ */
+class MaxBinaryHeap {
+    /**
+     * Create a Max Binary Heap.
+     */
+    constructor() {
+        this.heap = [];
+    }
+}
+
+export default MaxBinaryHeap;
