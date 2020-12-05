794bdc69299057d7f01598c6c9b26c3d076d77c6
Jose Quezada <quezada007@yahoo.com>
2020-12-04 20:40:04 -0800

    add hash function to Hash Table

--- src/data-structures/hash-tables/separate-chaining-hash-table.js
+++ src/data-structures/hash-tables/separate-chaining-hash-table.js
@@ -9,6 +9,22 @@ class HashTable {
         this.keyMap = new Array(size);
         this.hashTableSize = 0;
     }
+
+    /**
+     * Convert a key into a hash number.
+     * @param {string} key - The key for the hash table.
+     * @returns {number} - The hash number.
+     */
+    hash(key) { // O(1)
+        let total = 0;
+        const WEIRD_PRIME = 31;
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            const char = key[i];
+            const value = char.charCodeAt(0) - 96;
+            total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+        }
+        return total;
+    }
 }
 
 export default HashTable;
