6f95c342da0b45b67e91bcb30361c6ededddc3c2
Jose Quezada <quezada007@yahoo.com>
2020-12-08 08:51:56 -0800

    create Undirected Unweighted Graph

---
+++ src/data-structures/graphs/undirected-unweighted-graph.js
@@ -0,0 +1,13 @@
+/**
+ * Class representing an Undirected Unweighted Graph.
+ */
+class Graph {
+    /**
+     * Create an Undirected Unweighted Graph.
+     */
+    constructor() {
+        this.adjacencyList = {};
+    }
+}
+
+export default Graph;
