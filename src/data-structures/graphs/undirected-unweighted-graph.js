55a10c0a9e052ae751ef9dba441f5efd3b76e625
Jose Quezada <quezada007@yahoo.com>
2020-12-08 08:52:41 -0800

    add addVertex method to graph

--- src/data-structures/graphs/undirected-unweighted-graph.js
+++ src/data-structures/graphs/undirected-unweighted-graph.js
@@ -8,6 +8,18 @@ class Graph {
     constructor() {
         this.adjacencyList = {};
     }
+
+    /**
+     * Add a vertex to the graph.
+     * @param {string} vertex - The vertex to add to the graph.
+     */
+    addVertex(vertex) { // O(1)
+        // Check to make sure the vertex is not in the list
+        if (!this.adjacencyList[vertex]) {
+            // Add vertex as key with an empty array as the value
+            this.adjacencyList[vertex] = [];
+        }
+    }
 }
 
 export default Graph;
