afc0b10783915f2cd6ee1ca0c0ab9f97e1f65b8f
Jose Quezada <quezada007@yahoo.com>
2020-12-09 08:00:59 -0800

    add unit tests to addEdge method

--- src/data-structures/graphs/undirected-unweighted-graph.test.js
+++ src/data-structures/graphs/undirected-unweighted-graph.test.js
@@ -26,4 +26,56 @@ describe('Undirected Unweighted Graph', () => {
             'New York': []
         });
     });
+
+    test('adding an edge between vertices', () => {
+        const g = new Graph();
+        g.addVertex('Los Angeles');
+        g.addVertex('Dallas');
+        g.addEdge('Los Angeles', 'Dallas');
+        expect(g.adjacencyList).toMatchObject({
+            'Los Angeles': ['Dallas'],
+            Dallas: ['Los Angeles']
+        });
+        g.addEdge('Los Angeles', 'Dallas');
+        expect(g.adjacencyList).toMatchObject({
+            'Los Angeles': ['Dallas'],
+            Dallas: ['Los Angeles']
+        });
+        g.addVertex('New York');
+        g.addEdge('Dallas', 'New York');
+        expect(g.adjacencyList).toMatchObject({
+            'Los Angeles': ['Dallas'],
+            Dallas: ['Los Angeles', 'New York'],
+            'New York': ['Dallas']
+        });
+        g.addVertex('Chicago');
+        g.addEdge('Los Angeles', 'Chicago');
+        expect(g.adjacencyList).toMatchObject({
+            'Los Angeles': ['Dallas', 'Chicago'],
+            Dallas: ['Los Angeles', 'New York'],
+            'New York': ['Dallas'],
+            Chicago: ['Los Angeles']
+        });
+        g.addEdge('Dallas', 'Chicago');
+        expect(g.adjacencyList).toMatchObject({
+            'Los Angeles': ['Dallas', 'Chicago'],
+            Dallas: ['Los Angeles', 'New York', 'Chicago'],
+            'New York': ['Dallas'],
+            Chicago: ['Los Angeles', 'Dallas']
+        });
+        g.addEdge('Dallas', 'Miami');
+        expect(g.adjacencyList).toMatchObject({
+            'Los Angeles': ['Dallas', 'Chicago'],
+            Dallas: ['Los Angeles', 'New York', 'Chicago'],
+            'New York': ['Dallas'],
+            Chicago: ['Los Angeles', 'Dallas']
+        });
+        g.addEdge('Phoenix', 'Seattle');
+        expect(g.adjacencyList).toMatchObject({
+            'Los Angeles': ['Dallas', 'Chicago'],
+            Dallas: ['Los Angeles', 'New York', 'Chicago'],
+            'New York': ['Dallas'],
+            Chicago: ['Los Angeles', 'Dallas']
+        });
+    });
 });
