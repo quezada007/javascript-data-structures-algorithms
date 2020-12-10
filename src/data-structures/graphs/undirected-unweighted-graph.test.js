47e793c368d1b2023595f9492b82eb50a7db75ca
Jose Quezada <quezada007@yahoo.com>
2020-12-10 06:47:58 -0800

    add unit tests for removeVertex

--- src/data-structures/graphs/undirected-unweighted-graph.test.js
+++ src/data-structures/graphs/undirected-unweighted-graph.test.js
@@ -131,4 +131,46 @@ describe('Undirected Unweighted Graph', () => {
             Chicago: ['Los Angeles']
         });
     });
+
+    test('removing a vertex from the graph', () => {
+        const g = new Graph();
+        g.addVertex('Los Angeles');
+        g.addVertex('Dallas');
+        g.addEdge('Los Angeles', 'Dallas');
+        g.addVertex('New York');
+        g.addEdge('Dallas', 'New York');
+        g.addVertex('Chicago');
+        g.addEdge('Los Angeles', 'Chicago');
+        g.addEdge('Dallas', 'Chicago');
+        expect(g.adjacencyList).toMatchObject({
+            'Los Angeles': ['Dallas', 'Chicago'],
+            Dallas: ['Los Angeles', 'New York', 'Chicago'],
+            'New York': ['Dallas'],
+            Chicago: ['Los Angeles', 'Dallas']
+        });
+        g.removeVertex('Miami');
+        expect(g.adjacencyList).toMatchObject({
+            'Los Angeles': ['Dallas', 'Chicago'],
+            Dallas: ['Los Angeles', 'New York', 'Chicago'],
+            'New York': ['Dallas'],
+            Chicago: ['Los Angeles', 'Dallas']
+        });
+        g.removeVertex('Dallas');
+        expect(g.adjacencyList).toMatchObject({
+            'Los Angeles': ['Chicago'],
+            'New York': [],
+            Chicago: ['Los Angeles']
+        });
+        g.removeVertex('New York');
+        expect(g.adjacencyList).toMatchObject({
+            'Los Angeles': ['Chicago'],
+            Chicago: ['Los Angeles']
+        });
+        g.removeVertex('Los Angeles');
+        expect(g.adjacencyList).toMatchObject({
+            Chicago: []
+        });
+        g.removeVertex('Chicago');
+        expect(g.adjacencyList).toMatchObject({});
+    });
 });
