41613aa74d8b84575908510359f6d56f07da8eec
Jose Quezada <quezada007@yahoo.com>
2020-12-10 06:47:16 -0800

    add Depth First Traversal recursive

--- src/data-structures/graphs/undirected-unweighted-graph.js
+++ src/data-structures/graphs/undirected-unweighted-graph.js
@@ -65,6 +65,43 @@ class Graph {
             delete this.adjacencyList[vertex];
         }
     }
+
+    /**
+     * Depth First Traversal Recursive.
+     * @param {string} startingVertex - The starting vertex.
+     * @returns {Array} - An array containing all the visited nodes.
+     */
+    depthFirstTraversalRecursive(startingVertex) { // O(V+E)
+        // Create an array to store all the vertices
+        const results = [];
+        // Create a visited object to keep track of all the visited vertices
+        const visited = {};
+        // Get the adjacency list
+        const { adjacencyList } = this;
+        // Create a recursive helper function to traverse the graph
+        function dfs(vertex) {
+            // If no more neighbor vertices, then exit
+            if (!vertex || !adjacencyList[vertex]) {
+                return;
+            }
+            // Mark the vertex as visited
+            visited[vertex] = true;
+            // Add the visited vertex in the results array
+            results.push(vertex);
+            // Visit every edge in a vertex
+            adjacencyList[vertex].forEach((neighbor) => {
+                // If the neighbors of the vertex are not visited, then traverse them
+                if (!visited[neighbor]) {
+                    // Call the helper function recursively
+                    dfs(neighbor);
+                }
+            });
+        }
+        // Start the traversing at the starting vertex
+        dfs(startingVertex);
+        // Return the results array with all the vertices
+        return results;
+    }
 }
 
 export default Graph;
