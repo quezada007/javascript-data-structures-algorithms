877ad1cf391a863608d78cd56901017da19f9d11
Jose Quezada <quezada007@yahoo.com>
2020-12-12 14:05:41 -0800

    add addEdge method to graph

--- src/data-structures/graphs/undirected-weighted-graph.js
+++ src/data-structures/graphs/undirected-weighted-graph.js
@@ -20,6 +20,20 @@ class Graph {
             this.adjacencyList[vertex] = [];
         }
     }
+
+    /**
+     * Add an edge connecting 2 vertices.
+     * @param {string} vertex1 - The first vertex.
+     * @param {string} vertex2 - The second vertex.
+     * @param {number} weight - The weight of the edge.
+     */
+    addEdge(vertex1, vertex2, weight) { // O(1)
+        // Make sure the vertices are in the graph
+        if (this.adjacencyList[vertex1] && !this.adjacencyList[vertex1].includes(vertex2) && this.adjacencyList[vertex2]) {
+            this.adjacencyList[vertex1].push({ vertex: vertex2, weight });
+            this.adjacencyList[vertex2].push({ vertex: vertex1, weight });
+        }
+    }
 }
 
 export default Graph;
