ccdfaa5e0f9135b30991b8103fe90311fb12c93f
Jose Quezada <quezada007@yahoo.com>
2020-12-14 19:16:49 -0800

    create Priority Queue Array

---
+++ src/data-structures/stacks-queues/priority-queue-array.js
@@ -0,0 +1,13 @@
+/**
+ * Class representing a Priority Queue using an array.
+ */
+class PriorityQueueArray {
+    /**
+     * Create a Priority Queue Array.
+     */
+    constructor() {
+        this.queue = [];
+    }
+}
+
+export default PriorityQueueArray;
