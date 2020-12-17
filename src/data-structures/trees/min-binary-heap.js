e13853e7945448e3e709c6cc228b68e8d17e7457
Jose Quezada <quezada007@yahoo.com>
2020-12-17 06:43:41 -0800

    create Min Binary Heap class

---
+++ src/data-structures/trees/min-binary-heap.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-constant-condition */
+/**
+ * Class representing a Min Binary Heap.
+ */
+class MinBinaryHeap {
+    /**
+     * Create a Min Binary Heap.
+     */
+    constructor() {
+        this.heap = [];
+    }
+}
+
+export default MinBinaryHeap;
