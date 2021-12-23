6a1b30a7b4dfbeb1f4f416936dfe23dd2bc4f332
Jose Quezada <quezada007@yahoo.com>
2021-12-22 22:18:06 -0800

    update bubble sort

--- src/algorithms/sorting/bubble-sort.js
+++ src/algorithms/sorting/bubble-sort.js
@@ -9,12 +9,13 @@
  * @returns {Array} - A new sorted array.
  */
 export default function bubbleSort(unsortedArray) {
+    // Create a copy of the unsorted array
     const arr = [...unsortedArray];
-    let noSwaps;
     for (let i = arr.length; i > 0; i--) {
-        noSwaps = true;
+        let noSwaps = true;
         for (let j = 0; j < i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
+                // Swap values
                 [arr[j + 1], arr[j]] = [arr[j], arr[j + 1]];
                 noSwaps = false;
             }
